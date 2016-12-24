ed861c0e0cba44be0450458efb019625d38a2fe8
andrerpena <andrerpena@gmail.com>
2016-12-24 19:37:10 -0200

    Making progress

--- src/App.js
+++ src/App.js
@@ -27,6 +27,25 @@ const ButtonBar = () => {
     </div>
 }
 
+const Block = ({title, children}) => <div className="block">
+    <div className="header">
+        <span>
+            {title}
+        </span>
+    </div>
+    <div>
+        {children}
+    </div>
+</div>
+
+const Item = ({title, iconClass}) => {
+    let icon = icon ? <i className={`fa fa-${iconClass}`} aria-hidden="true"></i> : null;
+    return <div className="item">
+        <icon />
+        <span>{title}</span>
+    </div>
+}
+
 class App extends Component {
 
 
@@ -36,12 +55,67 @@ class App extends Component {
                 <Picture />
                 <MainHeader />
                 <ButtonBar />
-                <div className="header">
-                    Bio:
-                </div>
-                <p>
-                    I'm self-motivated and passionate about software development and technology in general. I enjoy learning new frameworks, libraries and tools. I like to participate in online communities like Stackoverflow and contribute to open-source on GitHub.
-                </p>
+                <Block title="Bio">
+                    <p>
+                        I'm self-motivated and passionate about software development and technology in general. I enjoy learning new frameworks, libraries and tools.
+                        I like to participate in online communities like Stackoverflow and contribute to open-source on GitHub. I have 10+ years of experience
+                        with software development.
+                    </p>
+                    <p>
+                        My hobbies are video-games, playing the guitar, traveling and photography.
+                    </p>
+                </Block>
+                <Block title="Current job">
+                    <p>
+                        <b>Software Architect</b> at <a href="http://thomsonreuters.com/en.html">Thomson Reuters</a>. Because this title has a different meaning at different places, basically
+                        I help designing the architecture for a web practice managament system for laywers called <a href="https://www.thomsonreuters.com.br/pt/juridico/legal-one.html">Legal One</a>. I also
+                        help integrating Thomson Reuters technologies across the globe, I review code, I create POCs for new technologies and I provide effort estimation for new requirements.
+                        I never stopped coding.
+                    </p>
+                </Block>
+                <Block title="Social Media">
+                    <div className="social-media-wrapper">
+                        <Item title="Stackoverflow" icon="stack-overflow" />
+                        <Item title="Stackoverflow Jobs" icon="stack-overflow" />
+                        <Item title="GitHub" icon="github" />
+                        <Item title="Medium" icon="medium" />
+                        <Item title="Linkedin" icon="linkedin" />
+                        <Item title="Facebook" icon="facebook" />
+                        <Item title="Twitter" icon="twitter" />
+                        <Item title="Hackerrank" icon="header" />
+                    </div>
+                </Block>
+                <Block title="Technologies I'm familiar with">
+                    <div className="technologies-wrapper">
+                        <Item title="C#" />
+                        <Item title=".NET" />
+                        <Item title="ASP.NET MVC" />
+                        <Item title="ASP.NET WebForms" />
+                        <Item title="JavaScript" />
+                        <Item title="Node.js" />
+                        <Item title="Python" />
+                        <Item title="HTML 5" />
+                        <Item title="CSS" />
+                        <Item title="SASS" />
+                        <Item title="Webpack" />
+                        <Item title="Babel" />
+                        <Item title="Git" />
+                        <Item title="TFS" />
+                    </div>
+                </Block>
+                <Block title="Contact">
+                    <div className="contact-wrapper">
+                        <Item title="+55 32 99916-8204" icon="whatsapp" />
+                        <Item title="andrerpena@gmail.com" icon="skype" />
+                        <Item title="andrerpena@gmail.com" icon="envelope" />
+                    </div>
+                </Block>
+                <Block title="Location">
+                    <iframe width="600" height="300" frameBorder="0" style={{ border: 0, marginTop: 5 }}
+                        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJT5tvl1-bmAAR7llGN0HiHvo&key=AIzaSyA6Wlf-UvJONIzGsyp-g53SMdgcgyLjQG4"></iframe>
+                </Block>
+
+
             </div>
         );
     }
