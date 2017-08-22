4a589524ade09cabf54509c2ce5f7deb7c9276c6
André Pena <andrerpena@gmail.com>
2017-08-22 07:34:07 -0300

    Deleting build

--- gulpfile.js
+++ gulpfile.js
@@ -7,29 +7,31 @@ const webpackConfig = require('./webpack.config.demo.prod.js');
 // removes the output configuration from the webpack.config.js file, otherwise it doesn't work.
 webpackConfig.output.path = null;
 
+const destinationFolder = './build'
+
 gulp.task('copy-index', function () {
     return gulp.src('./src/index.prod.html')
         .pipe(rename('index.html'))
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destinationFolder));
 });
 
 gulp.task('copy-cv', function () {
     return gulp.src('./cv/cv.pdf')
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destinationFolder));
 });
 
 gulp.task('copy-favicon', function () {
     return gulp.src('./src/favicon.ico')
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destinationFolder));
 });
 
 gulp.task('copy-cname', function () {
     return gulp.src('./CNAME')
-        .pipe(gulp.dest('./build'));
+        .pipe(gulp.dest(destinationFolder));
 });
 
 gulp.task('build', ['copy-index', 'copy-cv', 'copy-cname', 'copy-favicon'], function () {
     return gulp.src("./src/client.js")
         .pipe(webpack(webpackConfig))
-        .pipe(gulp.dest('./docs'));
+        .pipe(gulp.dest(destinationFolder));
 });
